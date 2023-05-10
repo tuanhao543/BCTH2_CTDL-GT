9ad1ccad1ab9d72217860fdc96dc65b87240af82
tuanhao543 <125726528+tuanhao543@users.noreply.github.com>
2023-05-10 11:29:31 +0700

    Add files via upload

---
+++ tichcacso.cpp
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+int main()
+
+{
+
+int i, n;
+
+long P;
+
+i = 1;
+
+P = 1;
+
+do
+
+{
+
+printf("\nNhap n: ");
+
+scanf("%d", &n);
+
+if(n < 1)
+
+{
+
+printf("\nN phai lon hon hoac bang 1. Xin nhap lai !");
+
+}
+
+}while(n < 1);
+
+while(i < n)
+
+{
+
+P = P * i;
+
+i++;
+
+}
+
+printf("\nTich 1x2x....x%d la: %ld", i, P);
+
+getch();
+
+return 0;
+
+}
